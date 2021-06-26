08df60a5dcd907241f15bfa10307344114fca365
denis-kaligon <denis.k@kaligon.com>
2021-06-26 13:09:32 +0300

    add desktop version

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,8 @@ module.exports = {
       colors: {
         'dark': "#181a1d",
         'primary': '#00a2d9',
-        'light' :'#e9e9e9'
+        'light': '#e9e9e9',
+        'light-blue':'#1F3649'
       }
     },
   },
