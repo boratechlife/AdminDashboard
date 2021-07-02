167caafcaef782db6947973f1df2a15e4da0543d
denis-kaligon <denis.k@kaligon.com>
2021-07-02 19:22:29 +0300

    finish index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,17 @@ module.exports = {
   darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {
+      fontSize: {
+        sm:"13px"
+      },
+
+          fontFamily: {
+        sans: ['Lato'],
+        serif: ['Open Sans']
+      },
       colors: {
-        'dark': "#181a1d",
+        'dark': "#181a1D",
+        'blueish':"#319DB5",
         'primary': '#00a2d9',
         'light': '#e9e9e9',
         'light-blue':'#1F3649'
